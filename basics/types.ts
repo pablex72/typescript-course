9b15770fa94eaa36c5cad5e8393673c10f0b1093
pablo <tonyp2hoy@gmail.com>
2025-08-29 00:13:23 -0400

    casting en ts

--- basics/types.ts
+++ basics/types.ts
@@ -21,13 +21,24 @@ let response44 = undefined;
 let response5: number | null = 5;
 response5?.toString(); //null safety
 
+//combinacion de tipos
 let responseProducts: number | undefined;
 
 type ResponseTypeService = string | undefined;
 
 let responseProducts333: ResponseTypeService;
 
+//type ASSERTION
+//OPCION 1
+let message:any = "";
+let messageUppercase = (message as string).toUpperCase();
+//OPCION 2
+let message2:any = "";
+let message2Uppercase = <string> message2;
+message2Uppercase.toUpperCase();
 
+const canvas = <HTMLCanvasElement> document.getElementById("canvas")
+canvas.CDATA_SECTION_NODE;
 
-
-
+//opciona 4
+const canvas2 = document.getElementById("canvas") as HTMLCanvasElement;
