8a492e9c4a8caa0c5791ab4690dc9d2754f90a96
pablo <tonyp2hoy@gmail.com>
2025-09-01 23:29:31 -0400

    adicionando private field & herencia

--- basics/clases.ts
+++ basics/clases.ts
@@ -1,9 +1,19 @@
 class Animal {
     name: "Generic animal";
+    private age = 10; // priavate cerrado al exterior pero se usa dentro de la clase
     sayHi(){
-        console.log("grrrrr");
+        console.log("grrrrr", this.age);
     }
 }
 
+
 const myAnimal: Animal = new Animal();
 myAnimal.sayHi();
+
+//herencia 
+
+class Dog extends Animal {
+    type: "Pastor Aleman";
+}
+const myDog: Dog = new Dog();
+myDog.type;
